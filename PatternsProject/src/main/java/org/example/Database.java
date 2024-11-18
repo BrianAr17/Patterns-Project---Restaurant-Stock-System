8da5aa55b25ed9b03c224be3f0c41d8d878fe9dd
KishaanT <kishaan2006@gmail.com>
2024-11-18 00:36:53 -0500

    Added dependencies in the vcs.xml file and created Database class to make the Restaurant Database. Made code for creation of Employee table with basic information required from Employee

--- PatternsProject/src/main/java/org/example/Database.java
+++ PatternsProject/src/main/java/org/example/Database.java
@@ -3,6 +3,7 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
     private static Connection connect() {
@@ -17,6 +18,25 @@ public class Database {
         return conn;
     }
 
+    // create Students table
+    public static void createStudentTable() {
+        String sql = " create table if not exists Employees (\n"
+                + " id integer primary key, \n"
+                + " name text not null, \n"
+                + " empPassword text not null\n"
+                + " age integer check (age >= 14)\n"
+                + " Position text not null\n"
+                + " empEmail text not null\n"
+                + " empPhoneNumber text not null\n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" students table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     public static void main(String[] args) {
 
 
