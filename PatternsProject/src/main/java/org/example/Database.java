efe4326ee94832b2763a5b9e368cf749045246f8
KishaanT <kishaan2006@gmail.com>
2024-11-18 20:41:05 -0500

    Creation of Perishable Food table, Order Records table and Kitchen Tools table. Gave Perishable Foods Table and Order Records Table appropriate columns. Kitchen Tools table's columns remain unchanged .

--- PatternsProject/src/main/java/org/example/Database.java
+++ PatternsProject/src/main/java/org/example/Database.java
@@ -18,7 +18,7 @@ public class Database {
         return conn;
     }
 
-    // create Students table
+    // create Employee table
     public static void createEmployeeTable() {
         String sql = " create table if not exists Employees (\n"
                 + " id integer primary key, \n"
@@ -32,7 +32,65 @@ public class Database {
         try(Connection conn = connect();
             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
-            System.out.println(" students table created successfully");
+            System.out.println(" Employee table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // create Perishable Food table
+    public static void createPerishableFoodsTable() {
+        String sql = " create table if not exists PerishableFoods (\n" // modify columns
+                + " foodId integer primary key, \n"
+                + " foodName text not null, \n"
+                + " Quantitu integer not null,\n"
+                + " Food_Desc text not null,\n"
+                + " Price/Lbs double not null,\n"
+                + " Supplier text not null,\n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("  Perishable Food table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // create Kitchen tools table
+    public static void createKitchenToolsTable() {
+        String sql = " create table if not exists KitchenTools (\n"
+                + " id integer primary key, \n"
+                + " name text not null, \n"
+                + " empPassword text not null,\n"
+                + " age integer check (age >= 14),\n"
+                + " Position text not null,\n"
+                + " empEmail text not null,\n"
+                + " empPhoneNumber text not null\n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" Kitchen Tools table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // create Order Records table
+    public static void createOrderRecordsTable() {
+        String sql = " create table if not exists OrderRecords (\n"
+                + " id integer primary key, \n"
+                + " ProductName text not null, \n"
+                + " ProductDesc text not null,\n"
+                + " Order_Date Date check (Order_Date <= DATE('now')),\n"
+                + " Arrival_Date Date not null check(Arrival_Date > Order_Date),\n"
+                + " OrderStatus text not null  check( OrderStatus = PENDING OR OrderStatus = RECEIVED OR Order_Status = DELIVERED)\n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" Order Records table created successfully");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
