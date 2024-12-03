690d4d86c7cd0292993beda531cf2dd6108e0ef4
KishaanT <kishaan2006@gmail.com>
2024-12-03 03:51:02 -0500

    updated the database.java file

--- PatternsProject/src/main/java/org/example/Database.java
+++ PatternsProject/src/main/java/org/example/Database.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Database {
     private static Connection connect() {
@@ -18,15 +15,15 @@ public class Database {
         return conn;
     }
 
-    // create Employee table
+    // create Employee table =============================================================================
     public static void createEmployeeTable() {
         String sql = " create table if not exists Employees (\n"
                 + " id integer primary key, \n"
                 + " name text not null, \n"
-                + " empPassword text not null\n"
-                + " age integer check (age >= 14)\n"
-                + " Position text not null\n"
-                + " empEmail text not null\n"
+                + " empPassword text not null,\n"
+                + " age integer check (age >= 14),\n"
+                + " Position text not null,\n"
+                + " empEmail text not null,\n"
                 + " empPhoneNumber text not null\n"
                 + ");";
         try(Connection conn = connect();
@@ -38,82 +35,158 @@ public class Database {
         }
     }
 
-    // create Perishable Food table
-    public static void createPerishableFoodsTable() {
-        String sql = " create table if not exists PerishableFoods (\n" // modify columns
-                + " foodId integer primary key, \n"
-                + " foodName text not null, \n"
-                + " Quantity integer not null,\n"
-                + " Food_Desc text not null,\n"
-                + " Price/Lbs double not null,\n"
-                + " Supplier text not null,\n"
-                + ");";
-        try(Connection conn = connect();
-            Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
-            System.out.println("  Perishable Food table created successfully");
+    public static void insertEmployees(String name, String password,int age,String position, String email,String phoneNumber) {
+        String sql = " insert into Employees (name,empPassword,age,Position,empEmail,empPhoneNumber) values (?, ?,?, ?,?, ?)";
+        try(Connection conn= connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setString(2, password);
+            pstmt.setInt(3, age);
+            pstmt.setString(4, position);
+            pstmt.setString(5, email);
+            pstmt.setString(6, phoneNumber);
+            pstmt.executeUpdate();
+            System.out.println("Employee data has been added successfully");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    // create Kitchen tools table
-    public static void createKitchenToolsTable() {
-        String sql = " create table if not exists KitchenTools (\n"
-                + " toolID integer primary key, \n"
-                + " toolType text not null, \n"
-                + " toolBrand text not null,\n"
-                + " toolDesc text not null,\n"
-                + " quantity integer not null check ( quantity > 0),\n"
-                + " costPerUnit integer not null check(costPerUnit > 0),\n"
-                + ");";
-        try(Connection conn = connect();
-            Statement stmt = conn.createStatement()) {
-            stmt.execute(sql);
-            System.out.println(" Kitchen Tools table created successfully");
+    public static void displayEmployees() {
+        String sql = "select * from Employees";
+        try (Connection conn = connect(); Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            System.out.println("Employees :");
+            while (rs.next()) {
+                System.out.println("Id : " + rs.getInt("id") +
+                        "\nName: " + rs.getString("name") +
+                        "\nPassword : " + rs.getInt("empPassword")+
+                        "\nAge: " + rs.getString("age") +
+                        "\nPosition: " + rs.getString("Position") +
+                        "\nEmail: " + rs.getString("empEmail") +
+                        "\nPhoneNumber: " + rs.getString("empPhoneNumber"));
+                System.out.println("\n");
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    // create Order Records table
-    public static void createOrderRecordsTable() {
-        String sql = " create table if not exists OrderRecords (\n"
-                + " orderID integer primary key, \n"
-                + " ProductName text not null, \n"
-                + " ProductDesc text not null,\n"
-                + " Order_Date Date check (Order_Date <= DATE('now')),\n"
-                + " Arrival_Date Date not null check(Arrival_Date > Order_Date),\n"
-                + " OrderStatus text not null  check( OrderStatus = PENDING OR OrderStatus = RECEIVED OR Order_Status = DELIVERED)\n"
+    // create Order table =============================================================================
+    public static void createOrderTable() {
+        String sql = " create table if not exists Order (\n" // modify columns
+                + " orderId integer primary key, \n"
+                + " status text not null, \n" //TODO assure that the status column will have the same values of chosen ENUMS for status of deliveries
+                + " dateSent date not null check( dateSent <= dateArrived AND dateSent >= currentDate),\n" //TODO Check if there is a way within SQLite to return the machines date
+                + " dateArrived data not null check (dateArrived >= dateSent)\n"
                 + ");";
         try(Connection conn = connect();
             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
-            System.out.println(" Order Records table created successfully");
+            System.out.println("  Order table created successfully");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
-    // create Deliverer table
+    // create Delivery tools table
+    public static void createDeliveryTable() {
+        String sql = " create table if not exists Delivery (\n"
+                + " DeliveryId integer primary key, \n"
+                + " orderId integer not null, \n"
+                + " status text not null,\n" //TODO assure that the status column will have the same values of chosen ENUMS for status of deliveries
+                + " dateShipped date  not null check(dateShipped <= dateReceived AND dateShipped >= currentDate),\n" //TODO Check if there is a way within SQLite to return the machines date
+                + " dateReceived date not null check(dateReceived >= dateShipped AND dateShipped >= currentDate),\n"
+                + " foreign key integer (orderId) references Order (orderId) on delete set null\n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" Delivery table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // create Deliverer table =============================================================================
     public static void createDelivererTable() {
         String sql = " create table if not exists Deliverer (\n"
                 + " delivererID integer primary key, \n"
-                + " ProductName text not null, \n"
-                + " ProductDesc text not null,\n"
-                + " Order_Date Date check (Order_Date <= DATE('now')),\n"
-                + " Arrival_Date Date not null check(Arrival_Date > Order_Date),\n"
-                + " OrderStatus text not null  check( OrderStatus = PENDING OR OrderStatus = RECEIVED OR Order_Status = DELIVERED)\n"
+                + " name text not null \n"
                 + ");";
         try(Connection conn = connect();
             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
-            System.out.println(" Order Records table created successfully");
+            System.out.println(" Deliverer table created successfully");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
 
+    // create Products Records table =============================================================================
+    public static void createProductsTable() {
+        String sql = " create table if not exists Products (\n"
+                + " productId integer primary key, \n"
+                + " productName text not null, \n"
+                + " productDesc text not null,\n"
+                + " pricePerUnit double not null check( pricePerUnit > 0),\n"
+                + " productType text not null check( productType = Persh OR productType = Non-Persh OR productType = Equip)\n" //TODO change the check variable to match the actual types of products
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" Products table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // create Receipt table =============================================================================
+    public static void createReceiptTable(){
+        String sql = " create table if not exists Receipt (\n"
+                +" receiptId integer primary key, \n"
+                +" orderId integer not null, \n"
+                +" productId integer not null,\n"
+                +" quantity integer not null check( quantity > 0),\n"
+                +" foreign key (orderId) references Order (orderId) on delete set null,\n"
+                +" foreign key (productId) references Products (productId) on delete set null \n"
+                + ");";
+        try(Connection conn = connect();
+            Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println(" Receipt table created successfully");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Methods ================================================================================
+
+    public static boolean checkForEmployeeValidity(String name, String password){
+        String sql = "select 1 " +
+                "from Employees " +
+                "where name LIKE ? ANd empPassword LIKE ?";
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql))
+
+        {
+            pstmt.setString(1,name);
+            pstmt.setString(2,password);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                System.out.println("Input valid");
+                return true;
+            }
+            else{
+                System.out.println("This user does not exists");
+                return false;
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
 
 
     public static void main(String[] args) {
